1d0e8fc3c657d4233a4e6c6ca55d35bf87b046ed
AliceAlfaioli <156820194+AliceAlfaioli@users.noreply.github.com>
2024-03-22 16:26:36 +0100

    secondo commit - modifiche

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
+
 import ComponentNavbar from "./components/ComponentNavbar";
 import ComponentMain from "./components/ComponentMain";
 import ComponentSearchBar from "./components/ComponentSearchBar";
-import ComponentDetail from "./components/ComponentDetail";
 import { useState } from "react";
+import ComponentDetail from "./components/ComponentDetail";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 function App() {
   const [city, setCity] = useState("");
   const [countryCode, setCountryCode] = useState("");
@@ -16,14 +17,14 @@ function App() {
     setCity(city);
     setCountryCode(countryCode);
   };
-
   return (
-    <div className="App">
-      <BrowserRouter>
+    <BrowserRouter>
+      <div className="App">
         <header>
           <ComponentNavbar />
         </header>
-        <div className="App-header">
+
+        <main className="App-header">
           <Routes>
             <Route
               element={
@@ -34,11 +35,15 @@ function App() {
               }
               path="/"
             />
+          </Routes>
+          <Routes>
             <Route element={<ComponentDetail city={city} countryCode={countryCode} />} path="/detail" />
           </Routes>
-        </div>
-      </BrowserRouter>
-    </div>
+        </main>
+
+        <footer></footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
